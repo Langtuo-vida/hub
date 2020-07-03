403a02bb2c0909cb4bad70b2dd6e6157d0d1bde9
Langtuo-Vida <vlangtuo@gmail.com>
2020-07-03 05:08:30 -0700

    initial commit

---
+++ hub/mypython.py
@@ -0,0 +1,12 @@
+
+myname = "vida langtuo"
+mynumber =500
+mybool = true
+
+
+def full_name(first, last):
+    return first + "" + last
+
+
+    full_name("vida", "langtuo")
+    # comments in python
